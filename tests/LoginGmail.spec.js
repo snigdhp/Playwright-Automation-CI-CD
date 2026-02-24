bc3a9ca747fbdcb8329eb3efb03d916eb662297d
snigdhp <snigdhpanwar1996@gmail.com>
2026-02-24 22:21:49 +0530

    Add new test files

--- tests/LoginGmail.spec.js
+++ tests/LoginGmail.spec.js
@@ -9,6 +9,12 @@ test ('GmailLogin', async ({page})=>{
      await expect(page).toHaveURL("https://workspace.google.com/intl/en-US/gmail/");
 
 
-})
-
-
+     
+     await page.getByRole('link', { name: 'Sign in' }).click();
+     await expect(page).toHaveURL("https://workspace.google.com/intl/en-US/gmail/");
+     test.setTimeout(120_000)
+     await page.getByLabel("Email or phone", "usep67705@gmail.com");
+     await page.locator('//*[@id="identifierNext"]/div/button/span').click();
+    //  await page.getByRole('button', { name: 'Next' }).click();
+      
+});
